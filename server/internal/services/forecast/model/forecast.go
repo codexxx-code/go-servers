d0413f376f74adf68274007952430a6c60186488
bonavi <124391691+bonavi@users.noreply.github.com>
2024-10-25 00:59:01 +0700

    off scheduling by env

--- server/internal/services/forecast/model/forecast.go
+++ server/internal/services/forecast/model/forecast.go
@@ -7,7 +7,7 @@ import (
 
 type Forecast struct {
 	ID     uint32        `db:"id" json:"id"`
-	Date   datetime.Date `db:"date" json:"date"`
+	Date   datetime.Date `db:"date" json:"date" swaggertype:"string" format:"date"`
 	Zodiac zodiac.Zodiac `db:"zodiac" json:"zodiac" enums:"aries,taurus,gemini,cancer,leo,virgo,libra,scorpio,sagittarius,capricorn,aquarius,pisces"`
 	Text   string        `db:"text" json:"text"`
 }
