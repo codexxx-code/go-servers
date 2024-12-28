ff06760383f292befeca3e1ae99b0af109b49fc6
bonavi <124391691+bonavi@users.noreply.github.com>
2024-12-28 21:36:59 +0700

    rm pkg

--- pkg/log/model/systemInfo.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type SystemInfo struct {
-	Hostname string `json:"hostname"`
-	Version  string `json:"version"`
-	Build    string `json:"build"`
-	Commit   string `json:"commit"`
-	Env      string `json:"env"`
-}
--- pkg/openrtb/auctionPriceMacros.go
+++
@@ -1,3 +0,0 @@
-package openrtb
-
-const AuctionPriceMacros = "${AUCTION_PRICE}"
